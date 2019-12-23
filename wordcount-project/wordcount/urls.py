ad08e7683b1949c10ce4c3de1874bc2932ce60f9
xuting1108 <xuting1108@hotmail.com>
2019-12-23 10:23:32 -0200

    Add new path

--- wordcount-project/wordcount/urls.py
+++ wordcount-project/wordcount/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('', views.home),
+    path('count/', views.count, name='count'),
 ]
