4b606af1f0131b3edddf9208dd85752a8fa600cf
xuting1108 <xuting1108@hotmail.com>
2019-12-26 10:14:16 -0200

    Add new method for call 'about'

--- wordcount-project/wordcount/views.py
+++ wordcount-project/wordcount/views.py
@@ -24,3 +24,6 @@ def count(request):
     sortedwords = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True)
 
     return render(request, "count.html", {'fulltext': fulltext, 'count':len(wordlist), 'sortedwords':sortedwords})
+
+def about(request):
+    return render(request, 'about.html')
