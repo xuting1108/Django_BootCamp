97ab066ce79ed053d37e99c5a64f53fa61de8713
xuting1108 <xuting1108@hotmail.com>
2019-12-27 12:46:38 -0200

    Add media root and media url in settings

--- portfolio - project/portfolio/settings.py
+++ portfolio - project/portfolio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jobs.apps.JobsConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
