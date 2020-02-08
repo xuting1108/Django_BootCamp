6decd4febab6840d1dad0ce34e6ed379bffe879a
xuting1108 <xuting1108@hotmail.com>
2020-02-08 08:31:46 -0200

    Add new urls and add apps and templates in settings

--- producthunt-project/producthunt/urls.py
+++ producthunt-project/producthunt/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from products.views import views
+from products import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
