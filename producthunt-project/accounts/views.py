0e251d8b7ad5b7e44516c88a1481b97d7092ed3e
Filipe Valadares <xuting1108@hotmail.com>
2020-02-22 09:48:16 -0300

    Login system (#2)
    * Fix name
    
    * Modify load staticfiles
    
    * Sign Up system
    
    * Changes in html
    
    * Add verifications in login and logout
    
    * Add changes login and logout html

--- producthunt-project/accounts/views.py
+++ producthunt-project/accounts/views.py
@@ -1,10 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
-def singup(request):
-    render(request, 'accounts/signup.html')
+def signup(request):
+    if request.method == 'POST':
+        #The user wants to sign up
+        #If the user exists and the password match
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'Username has already been taken'})
+            #If the user doesnotexist, create user and redirect to home
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+            else:
+                return render(request, 'accounts/signup.html', {'error': 'Password must match'})
+
+    else:
+        #User wants to enter info
+        return render(request, 'accounts/signup.html')
 
 def login(request):
-    render(request, 'accounts/login.html')
-
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password1'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'Username has already been taken'})
+    else:
+        return render(request, 'accounts/login.html')
+    
 def logout(request):
-    render(request, 'accounts/logout.html')
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
